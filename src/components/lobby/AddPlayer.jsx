300a1f4546756123b45d4c63767bf9cb63f405cf
SarTa10 <41445812+SarTa10@users.noreply.github.com>
2026-02-07 17:08:57 +0400

    added empty files for lobby

---
+++ src/components/lobby/AddPlayer.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const AddPlayer = () => {
+    return (
+        <div>
+            <h1>Add Player</h1>
+        </div>
+    )
+}
+
+export default AddPlayer;
---
+++ src/components/lobby/Players.jsx
@@ -0,0 +1,14 @@
+const Players = () => {
+ return (
+  <div>
+    <h2>Players</h2>
+    <ul>
+      <li>Player 1</li>
+      <li>Player 2</li>
+      <li>Player 3</li>
+    </ul>
+  </div>
+ )   
+}
+
+export default Players;
